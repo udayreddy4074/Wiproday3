3fc582153de0bafc2b48c19919983b488d457ad5
udayreddy4074 <201fa04074@gmail.com>
2025-01-29 11:12:15 +0530

    Add files via upload

---
+++ loopingday2.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 10;
+        for (; ; )
+        {
+            if (a <= 0)
+            {
+                break;
+            }
+            else
+            {
+                a = a - 2;
+
+
+            }
+            Console.WriteLine(a);
+
+        }
+        int x = 0;
+        while (x < 5)
+        {
+            x++;
+            if (x == 2)
+            {
+                break;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
