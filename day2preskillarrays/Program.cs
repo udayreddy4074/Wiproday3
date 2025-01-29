a43f054b404efdd95815cd083cb41519ed9320d7
udayreddy4074 <201fa04074@gmail.com>
2025-01-29 11:40:32 +0530

    Add files via upload

---
+++ day2preskillarrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using day2democlass;
+
+namespace democlass
+{
+    internal class Program
+    {
+        private static void Main(string[] args)// for defult contrutor and also paramters are already defined
+        {
+            employee emp = new employee();
+            emp.emp_id = 999;
+            emp.name = "uday";
+            Console.WriteLine($" Employee id:{emp.emp_id}");
+            Console.WriteLine($" Employee name:{emp.name}");
+
+
+            Console.WriteLine("Enter employee name");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Enter employee id");
+            int id = Convert.ToInt32(Console.ReadLine());
+            employee emp2 = new employee(id, name);
+            Console.WriteLine($" Employee id:{emp2.emp_id}");
+            Console.WriteLine($" Employee id:{emp2.name}");
+
+
+
+
+        }
+    }
+}
---
+++ day2preskillarrays/employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2democlass
+{
+    internal class employee
+    {
+        public int emp_id { get; set; }
+        public string name {  get; set; }   
+
+        public employee(int eid,string ename)//parameterized constructor 
+        {
+            emp_id = eid;
+            name = ename;
+
+        }
+        public employee() //default constuctor
+        {
+            Console.WriteLine("default constuctor");
+        }
+    }
+}
